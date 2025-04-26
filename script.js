a08dd08841a38f2451eeb778346232da42d093f8
Simeon Rediger <simeonrediger@gmail.com>
2025-04-26 05:29:46 -0400

    Add pseudocode for simulating a round

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// CREATE score trackers
+
+// Play a round
+    // GET human choice
+    // GET computer choice
+    // INCREMENT winner's score
+        // IF choices are the same
+            // End the round
+        // ELSE IF human chose rock
+            // IF computer chose paper
+                // INCREMENT computer's score
+            // ELSE
+                // INCREMENT human's score
+        // ELSE IF human chose paper
+            // IF computer chose rock
+                // INCREMENT human's score
+            // ELSE
+                // INCREMENT computer's score
+        // ELSE IF human chose scissors
+            // IF computer chose rock
+                // INCREMENT computer's score
+            // ELSE
+                // INCREMENT human's score
+
 function getComputerChoice() {
     const arbitraryIndex = Math.floor(Math.random() * 3);
 
