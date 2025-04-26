3c5c5e3da5b6aa14ad00e9eb9ece0c70c76e95a8
Simeon Rediger <simeonrediger@gmail.com>
2025-04-26 05:03:21 -0400

    Add pseudocode to simulate the computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+// GET 'rock', 'paper', or 'scissors'
+    // GET a random value greater than or equal to 0 and less than 1
+    // MULTIPLY it by 3
+    // GET its floor value to obtain our index value
+    // IF the index value is 0
+        // RETURN 'rock'
+    // ELSE IF the index value is 1
+        // RETURN 'paper'
+    // ELSE
+        // RETURN 'scissors'
