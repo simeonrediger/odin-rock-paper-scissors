548a8cd48ffc0f392fdf0ed768eabf10ba43710b
Simeon Rediger <simeonrediger@gmail.com>
2025-04-26 05:51:37 -0400

    Add function simulate one round

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
-// CREATE score trackers
+let humanScore = 0;
+let computerScore = 0;
 
-// Play a round
-    // GET human choice
-    // GET computer choice
-    // INCREMENT winner's score
-        // IF choices are the same
-            // End the round
-        // ELSE IF human chose rock
-            // IF computer chose paper
-                // INCREMENT computer's score
-            // ELSE
-                // INCREMENT human's score
-        // ELSE IF human chose paper
-            // IF computer chose rock
-                // INCREMENT human's score
-            // ELSE
-                // INCREMENT computer's score
-        // ELSE IF human chose scissors
-            // IF computer chose rock
-                // INCREMENT computer's score
-            // ELSE
-                // INCREMENT human's score
+function playRound() {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+
+    if (humanChoice === computerChoice) {
+        return;
+    }
+
+    const humanChoiceIndex = getChoiceIndex(humanChoice);
+    const computerChoiceIndex = getChoiceIndex(computerChoice);
+
+    if (humanChoiceIndex > computerChoiceIndex) {
+
+        if (humanChoiceIndex === 1) {
+            humanScore++;
+        } else {
+            computerScore++;
+        }
+
+    } else {
+
+        if (computerChoiceIndex === 1) {
+            computerScore++;
+        } else {
+            humanScore++;
+        }
+    }
+}
+
+function getHumanChoice() {
+    let userInput = prompt('Rock, Paper, Scissors, shoot!');
+    return userInput.toLowerCase();
+}
 
 function getComputerChoice() {
-    const arbitraryIndex = Math.floor(Math.random() * 3);
+    const randomChoiceIndex = Math.floor(Math.random() * 3);
 
-    switch (arbitraryIndex) {
+    switch (randomChoiceIndex) {
         case 0:
             return 'rock';
         case 1:
@@ -35,7 +48,14 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let userInput = prompt('Rock, Paper, Scissors, shoot!');
-    return userInput.toLowerCase();
+function getChoiceIndex(choice) {
+
+    switch (choice) {
+        case 'rock':
+            return 0;
+        case 'paper':
+            return 1;
+        case 'scissors':
+            return 2;
+    }
 }
